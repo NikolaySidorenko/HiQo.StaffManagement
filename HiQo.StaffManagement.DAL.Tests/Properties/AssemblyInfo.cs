f7c742f606bb50731652a86213bacfdbb33af402
Klaus <nikolaysidorenk99@gmail.com>
2018-07-20 21:05:10 +0300

    Craete simple tests

---
+++ HiQo.StaffManagement.DAL.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HiQo.StaffManagement.DAL.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HiQo.StaffManagement.DAL.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63f5dee0-e8aa-409b-a3a9-64ead46d1a16")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HiQo.StaffManagement.DAL.Tests/UserRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HiQo.StaffManagement.DAL.Database;
+using HiQo.StaffManagement.DAL.Database.Entities;
+using HiQo.StaffManagement.DAL.Repositories;
+using HiQo.StaffManagement.Domain.EntitiesDTO;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace HiQo.StaffManagement.DAL.Tests
+{
+
+    public class UserRepositoryTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            ConfigureAutomapper();
+
+            UserDTO user;
+            using (var dbContext = new CompanyContext())
+            {
+                UserRepository userRepository = new UserRepository(dbContext);
+                user = userRepository.GetById<User,UserDTO>(3);
+            }
+
+            Assert.AreEqual(3,user.UserId);
+        }
+
+        private static void ConfigureAutomapper()
+        {
+            Mapper.Initialize(GetConfiguration);
+        }
+
+        private static void GetConfiguration(IMapperConfigurationExpression configuration)
+        {
+            configuration.AddProfile(typeof(UserProfiler));
+        }
+    }
+}
